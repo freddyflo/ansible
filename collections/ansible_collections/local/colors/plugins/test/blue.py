2417db1bfcc71f4e066e9f8931cc8bd653050d97
Fred AKLAMANU <fred.aklamanu@ibm.com>
2023-07-24 16:13:33 +0200

    collections+plugin

---
+++ collections/ansible_collections/local/colors/plugins/test/blue.py
@@ -0,0 +1,24 @@
+# Ansible custom 'blue' test plugin definition
+
+def is_blue(string):
+    blue_values = [
+        'blue',
+        '#0000ff',
+        '#00f',
+        'rgb(0,0,255)',
+        'rgb(0%,0%,100%)'
+    ]
+    if string in blue_values:
+        return True
+    else:
+        return False
+
+class TestModule(object):
+    ''' Ansible blue test '''
+
+    def tests(self):
+        return {
+            'blue': is_blue
+        }
+        pass
+    pass
